f7cc0a74efc4a014dfac20647c9c045be83a1d87
Paweł Chłąd <pchlad5@gmail.com>
2022-11-24 10:26:23 +0100

    wip

--- solver.py
+++ solver.py
@@ -1,7 +1,8 @@
 from typing import List
 from parser import AST, BinOp, Num, Parser
 from lexer import TokenType
-
+from utils import inorder, trace 
+from functools import reduce
 
 class SolverException(Exception):
     pass
@@ -11,7 +12,7 @@ class Solver:
     def __init__(self, string: str):
         self.root = Parser(string).parse()
 
-    def solve(self, symbol: str):
+    def solve(self, symbol: str) -> str:
         """Solves for given _symbol_
         Solving is essentialy a process of moving all non _symbol_ nodes to the right side of the tree
         while having all _symbol_ nodes on the left side. This needs to be done with rules of algebra"""
@@ -21,6 +22,20 @@ class Solver:
             raise SolverException("Provided string is not an equation")
 
         # perform DFS to find requested symbol occurences
+        left_subtree_syms = self.dfs(symbol,self.root.left)
+        right_subtree_syms = self.dfs(symbol,self.root.right)
+
+        #swap left with right if there are more searched symbols on the right side
+        if len(right_subtree_syms) > len(left_subtree_syms):
+            self.root.left, self.root.right = self.root.right, self.root.left
+            left_subtree_syms, right_subtree_syms = right_subtree_syms, left_subtree_syms
+
+        #1. Get all searched symbols to the left side
+        #2. Get all not-searched symbols to the right side
+
+
+        return trace(self.root)
+        
 
     def dfs(self, symbol: str, start_point: AST = None) -> List[AST]:
         """Searches the ast tree
@@ -44,3 +59,4 @@ def test_dfs():
     s = Solver("a + b = a + c")
     res = s.dfs("a")
     assert len(res) == 2
+
