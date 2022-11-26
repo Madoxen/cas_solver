604a5118899217c2cb055a4d8d9bbe0a001b7611
Paweł Chłąd <pchlad5@gmail.com>
2022-11-26 14:01:38 +0100

    Moved parser tests to their own file, added conftest.py for automatic test discovery

--- parser.py
+++ parser.py
@@ -93,20 +93,4 @@ class Parser:
 
     def parse(self):
         return self.equation()
-
-
-def test_parser():
-    p = Parser("a + b = c")
-    result = p.parse()
-
-    #should be parsed as
-    #     =
-    #    / \
-    #   +   c
-    #  / \
-    # a   b
-    assert (result.op.type == TokenType.EQ 
-    and result.left.op.type == TokenType.PLUS
-    and result.left.left.token.type == TokenType.SYM
-    and result.left.right.token.type == TokenType.SYM
-    and result.right.token.type == TokenType.SYM)
+    
---
+++ parser_test.py
@@ -0,0 +1,18 @@
+from parser import Parser
+from lexer import TokenType
+
+def test_parser():
+    p = Parser("a + b = c")
+    result = p.parse()
+
+    #should be parsed as
+    #     =
+    #    / \
+    #   +   c
+    #  / \
+    # a   b
+    assert (result.op.type == TokenType.EQ 
+    and result.left.op.type == TokenType.PLUS
+    and result.left.left.token.type == TokenType.SYM
+    and result.left.right.token.type == TokenType.SYM
+    and result.right.token.type == TokenType.SYM)
