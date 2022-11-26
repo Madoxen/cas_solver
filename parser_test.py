2915b71a3a4758d230623b74816927211c10dada
Paweł Chłąd <pchlad5@gmail.com>
2022-11-26 14:10:14 +0100

    Added parser parenthesis test

--- parser_test.py
+++ parser_test.py
@@ -16,3 +16,21 @@ def test_parser():
     and result.left.left.token.type == TokenType.SYM
     and result.left.right.token.type == TokenType.SYM
     and result.right.token.type == TokenType.SYM)
+
+
+
+def test_parser_parenthesis():
+    p = Parser("(a + b) * c")
+    result = p.parse()
+
+    #should be parsed as
+    #     *
+    #    / \
+    #   +   c
+    #  / \
+    # a   b
+    assert (result.op.type == TokenType.MUL 
+    and result.left.op.type == TokenType.PLUS
+    and result.left.left.token.type == TokenType.SYM
+    and result.left.right.token.type == TokenType.SYM
+    and result.right.token.type == TokenType.SYM)
