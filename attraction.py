e32bc63059a96e5c78227020bcd092e4b6f99a81
Paweł Chłąd <pchlad5@gmail.com>
2023-01-08 17:50:13 +0100

    Added far attraction variant

--- attraction.py
+++ attraction.py
@@ -15,11 +15,11 @@ def attract_add_sub_mul(start_node: AST) -> bool:
     #     / \               / \
     #    a   b             a   b
     #              OR
-    #        *             *
-    #       / \           / \
-    #      *  c   OR    c    *
-    #     / \               / \
-    #    a   b             a   b
+    #        *              *
+    #       / \            / \
+    #      *  c    OR    c    *
+    #     / \                / \
+    #    a   b              a   b
     try:
         # Search rule:
         left_sided_pattern = create_compound_binop({TokenType.PLUS, TokenType.MINUS},
@@ -65,29 +65,45 @@ def attract_add_sub_mul(start_node: AST) -> bool:
     # Compute distance array
     unknowns_distances = [distance(x, y) for x,y in combinations(unknowns,2)] 
 
-    # Apply transformation
-    # replace b with c and c with b
-    if left_sided:
-        swap(start_node.left.right, start_node.right)
-    else:
-        swap(start_node.right.left, start_node.left)
 
-    # calculate distances again and compare
-    new_unknowns_distances = [distance(x, y) for x,y in combinations(unknowns,2)] 
-
-    # calculate distance change, if there is at least one negative change (so the two occurences of the same
-    # variable are closer) accept the change, otherwise reverse changes to the tree
-    distance_difference = []
-    for old_dist, new_dist in zip(unknowns_distances, new_unknowns_distances):
-        distance_difference.append(new_dist - old_dist)
-
-    success = any(x < 0 for x in distance_difference) 
-
-    if not success:
+    for far in [False, True]:
+        # Apply transformation
         if left_sided:
-            swap(start_node.left.right, start_node.right)
+            if not far:
+                swap(start_node.left.right, start_node.right)
+            else:
+                swap(start_node.left.left, start_node.right)
         else:
-            swap(start_node.right.left, start_node.left)
+            if not far:
+                swap(start_node.right.left, start_node.left)
+            else:
+                swap(start_node.right.right, start_node.left)
+
+        # calculate distances again and compare
+        new_unknowns_distances = [distance(x, y) for x,y in combinations(unknowns,2)] 
+
+        # calculate distance change, if there is at least one negative change (so the two occurences of the same
+        # variable are closer) accept the change, otherwise reverse changes to the tree
+        distance_difference = []
+        for old_dist, new_dist in zip(unknowns_distances, new_unknowns_distances):
+            distance_difference.append(new_dist - old_dist)
+
+        success = any(x < 0 for x in distance_difference) 
+
+        if not success:
+            #reverse transformation
+            if left_sided:
+                if not far:
+                    swap(start_node.left.right, start_node.right)
+                else:
+                    swap(start_node.left.left, start_node.right)
+            else:
+                if not far:
+                    swap(start_node.right.left, start_node.left)
+                else:
+                    swap(start_node.right.right, start_node.left)
+        else:
+            break
     return success
 
 
